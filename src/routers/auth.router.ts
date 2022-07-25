355f0c74b60210036ec3a9b19fce6fc701769dc2
Hiann <hiannmanso@email.com>
2022-07-25 14:35:35 -0300

    fix bugs deploy

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -2,9 +2,10 @@ import { Router } from 'express'
 import { signinGET, signupPOST } from '../controllers/auth.controller.js'
 import { validateSchema } from '../middlewares/validateSchema.js'
 import { authSchema } from '../schemas/auth.schema.js'
+import { signInSchema } from '../schemas/sigin.schema.js'
 
 const authRouter = Router()
 
 authRouter.post('/signup', validateSchema(authSchema), signupPOST)
-authRouter.post('/signin', signinGET)
+authRouter.post('/signin', validateSchema(signInSchema), signinGET)
 export default authRouter
